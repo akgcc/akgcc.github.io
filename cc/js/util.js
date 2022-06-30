d15efc3fb07f83c6015234fa7a5a0c0b20f37ce4
NeverDecaf <jtr9289@gmail.com>
2022-06-30 11:48:55 -0400

    add some new sounds

--- cc/js/util.js
+++ cc/js/util.js
@@ -24,7 +24,7 @@ const CLASS_MAPPING = {
 };
 const SHORT_NAMES = {
   "Skadi the Corrupting Heart": "Skadiva",
-  "Ch'en the Holungday": "Ch'en (Wet)",
+  "Ch'en the Holungday": "Ch'oom",
   "Nearl the Radiant Knight": "NTR",
 };
 const GAMEPRESS_NAME_MAP = { "Rosa (Poca)": "Rosa" };
